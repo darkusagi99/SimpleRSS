4c5643b8d277f5b5f9c953844478164adc35455c
darkusagi <darkusagi99@gmail.com>
2021-01-23 18:08:29 +0100

    Add feed in List

--- app/src/main/java/com/gmail/darkusagi99/simplerss/ItemFeedActivity.kt
+++ app/src/main/java/com/gmail/darkusagi99/simplerss/ItemFeedActivity.kt
@@ -1,5 +1,7 @@
 package com.gmail.darkusagi99.simplerss
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.widget.NestedScrollView
@@ -11,7 +13,9 @@ import com.google.android.material.snackbar.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 import com.gmail.darkusagi99.simplerss.dummy.DummyContent
 
@@ -39,29 +43,45 @@ class ItemFeedActivity : AppCompatActivity() {
         toolbar.title = title
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+            val taskEditText =  EditText(view.context)
+            val dialogClickListener =
+                DialogInterface.OnClickListener { _, which ->
+                    when (which) {
+                        DialogInterface.BUTTON_POSITIVE -> {
+                            Toast.makeText(this, "Création - " + taskEditText.text.toString(), Toast.LENGTH_SHORT).show()
+                            FeedConfig.addFeed(taskEditText.text.toString())
+                        }
+                        DialogInterface.BUTTON_NEGATIVE -> {
+                            Toast.makeText(this, "Pas de Création - " + taskEditText.text.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            val ab: AlertDialog.Builder = AlertDialog.Builder(view.context)
+            ab.setMessage("Ajouter nouveau flux RSS ?")
+                .setView(taskEditText)
+                .setPositiveButton("Oui", dialogClickListener)
+                .setNegativeButton("Non", dialogClickListener).show()
         }
 
         setupRecyclerView(findViewById(R.id.item_list))
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEM_MAP.values.toList())
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, FeedConfig.FEED_MAP.values.toList())
     }
 
     class SimpleItemRecyclerViewAdapter(private val parentActivity: ItemFeedActivity,
-                                        private val values: List<DummyContent.DummyItem>) :
+                                        private val values: List<FeedConfig.FeedItem>) :
             RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
 
         private val onClickListener: View.OnClickListener
 
         init {
             onClickListener = View.OnClickListener { v ->
-                val item = v.tag as DummyContent.DummyItem
+                val item = v.tag as FeedConfig.FeedItem
 
                 val intent = Intent(v.context, ItemDetailActivity::class.java).apply {
-                    putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id)
                 }
                 v.context.startActivity(intent)
             }
@@ -75,8 +95,8 @@ class ItemFeedActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val item = values[position]
-            holder.idView.text = item.id
-            holder.contentView.text = item.content
+            holder.idView.text = item.url
+            holder.contentView.text = item.url
 
             with(holder.itemView) {
                 tag = item
